078472178961605881325491e320cbbe4da3ac00
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-07 02:34:18 -0300

    commit

---
+++ src/main/java/com/apirestproducts/ApiRestProductsApplication.java
@@ -0,0 +1,13 @@
+package com.apirestproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestProductsApplication.class, args);
+    }
+
+}
