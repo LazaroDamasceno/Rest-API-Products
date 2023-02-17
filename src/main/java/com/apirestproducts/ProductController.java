93d7707aff883079452a0f766bc630de135a353e
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-17 01:37:29 -0300

    commit

--- src/main/java/com/apirestproducts/ProductController.java
+++ src/main/java/com/apirestproducts/ProductController.java
@@ -32,6 +32,41 @@ public class ProductController {
         return prodRep.save(product);
     }
 
+    @PutMapping("/change/name/{id}/{name}")
+    public ProductEntity replaceNameById(@PathVariable(value = "id") int id, @PathVariable(value = "name") String name) {
+        ProductEntity product = showById(id);
+        product.setName(name);
+        return prodRep.save(product);
+    }
+
+    @PutMapping("/change/brand/{id}/{brand}")
+    public ProductEntity replaceBrandById(@PathVariable(value = "id") int id, @PathVariable(value = "brand") String brand) {
+        ProductEntity product = showById(id);
+        product.setBrand(brand);
+        return prodRep.save(product);
+    }
+
+    @PutMapping("/change/type/{id}/{type}")
+    public ProductEntity replaceTypeById(@PathVariable(value = "id") int id, @PathVariable(value = "type") String type) {
+        ProductEntity product = showById(id);
+        product.setType(type);
+        return prodRep.save(product);
+    }
+
+    @PutMapping("/change/price/{id}/{type}")
+    public ProductEntity replaceTypeById(@PathVariable(value = "id") int id, @PathVariable(value = "type") int price) {
+        ProductEntity product = showById(id);
+        product.setPrice(price);
+        return prodRep.save(product);
+    }
+
+    @PutMapping("/change/quantity/{id}/{quantity}")
+    public ProductEntity replaceQuantityById(@PathVariable(value = "id") int id, @PathVariable(value = "quantity") int quantity) {
+        ProductEntity product = showById(id);
+        product.setQuantity(quantity);
+        return prodRep.save(product);
+    }
+
     @DeleteMapping("/delete")
     public void delete(@RequestBody ProductEntity product) {
         prodRep.delete(product);
